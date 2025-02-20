017cb24acdebb1966aed0fe49e71c090b4347724
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-02-19 23:01:30 -0600

    Add Keypad Layer

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/Ross-Thanscheidt/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Ross-Thanscheidt/keymap.c
@@ -22,47 +22,57 @@ enum layers{
   MAC_BASE,
   MAC_FN,
   WIN_BASE,
-  WIN_FN
+  WIN_FN,
+  WIN_KP
 };
 
 enum custom_keycodes {
     CKC_MAC = NEW_SAFE_RANGE,
     CKC_PC,
+    CKC_KP,
     CKC_TERM
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_tkl_ansi(
-     KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,  KC_VOLU,              KC_SNAP,  KC_SIRI,  RGB_MOD,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,         KC_L,     KC_SCLN,      KC_QUOT,              KC_ENT,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,              KC_RSFT,            KC_UP,
-     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                    KC_RCMMD,     KC_ROPTN, MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,   KC_MUTE,      KC_VOLD,    KC_VOLU,                KC_SNAP,  KC_SIRI,  RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,      KC_0,         KC_MINS,    KC_EQL,       KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,      KC_P,         KC_LBRC,    KC_RBRC,      KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,         KC_L,      KC_SCLN,      KC_QUOT,                  KC_ENT,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,   KC_DOT,       KC_SLSH,                  KC_RSFT,            KC_UP,
+     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                     KC_RCMMD,     KC_ROPTN,   MO(MAC_FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [MAC_FN] = LAYOUT_tkl_ansi(
-     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,   KC_F12,               KC_TRNS,  KC_TRNS,  RGB_TOG,
-     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_MAC,  RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,  RGB_M_G,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  DF(WIN_BASE), KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,            RGB_TOG,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                   KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD),
+     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,     KC_F10,       KC_F11,     KC_F12,                 KC_TRNS,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_MAC,  RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN,  RGB_M_K,      RGB_M_X,    RGB_M_G,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   DF(WIN_BASE), KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, KC_NO,        KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,            RGB_TOG,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD),
 
 [WIN_BASE] = LAYOUT_tkl_ansi(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,   KC_F12,               KC_PSCR,  KC_SCRL,  KC_PAUS,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
-     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,         KC_L,     KC_SCLN,      KC_QUOT,              KC_ENT,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,              KC_RSFT,            KC_UP,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                    KC_RALT,      KC_APP,   MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,     KC_F10,       KC_F11,     KC_F12,                 KC_PSCR,  KC_SCRL,  KC_PAUS,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,      KC_0,         KC_MINS,    KC_EQL,       KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,      KC_P,         KC_LBRC,    KC_RBRC,      KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
+     KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,         KC_L,      KC_SCLN,      KC_QUOT,                  KC_ENT,
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,   KC_DOT,       KC_SLSH,                  KC_RSFT,            KC_UP,
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                     KC_RALT,      KC_APP,     MO(WIN_FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_tkl_ansi(
-     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,  KC_VOLU,             CKC_TERM,  KC_MYCM,  KC_CALC,
-     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_PC,   RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,  RGB_M_G,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_NO,        KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, DF(MAC_BASE), KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,            RGB_TOG,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                   KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD)
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,   KC_MUTE,      KC_VOLD,    KC_VOLU,                CKC_TERM, KC_MYCM,  KC_CALC,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_PC,   RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN,  RGB_M_K,      RGB_M_X,    RGB_M_G,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_NO,        KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, DF(MAC_BASE), KC_TRNS,   KC_TRNS,      DF(WIN_KP),               KC_TRNS,            RGB_TOG,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD),
+
+[WIN_KP] = LAYOUT_tkl_ansi(
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NUM,   KC_PSLS,  KC_PAST, KC_MINUS,     KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,                KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  CKC_KP,   KC_TRNS,  KC_TRNS,  KC_KP_7, KC_KP_8,      KC_KP_9,   KC_PPLS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_4, KC_KP_5,      KC_KP_6,   KC_PPLS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_1, KC_KP_2,      KC_KP_3,   KC_PENT,      KC_TRNS,                  KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_KP_0,      KC_KP_0,   KC_PDOT,      KC_PENT,                  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    DF(WIN_BASE), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
 
 };
 
@@ -78,6 +88,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CKC_PC:
             if (record->event.pressed) {
                 SEND_STRING("PC");
+            }
+            break;
+
+        case CKC_KP:
+            if (record->event.pressed) {
+                SEND_STRING("Keypad");
             }
             break;
 
