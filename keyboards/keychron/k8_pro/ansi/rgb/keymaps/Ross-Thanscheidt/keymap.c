08386d004263af840206ef7b5fb1fc84f02face2
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-02-19 21:14:59 -0600

    Set default RGB mode, turn on NKRO by default, remap keys

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/Ross-Thanscheidt/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Ross-Thanscheidt/keymap.c
@@ -15,13 +15,9 @@
  */
 
 #include QMK_KEYBOARD_H
-
-extern keymap_config_t keymap_config;
-
 #include "k8_pro.h"
 #include "process_combo.h"
 
-// clang-format off
 enum layers{
   MAC_BASE,
   MAC_FN,
@@ -37,36 +33,36 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_tkl_ansi(
-     KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,  KC_VOLU,             KC_SNAP,  KC_SIRI,  RGB_MOD,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,  KC_EQL,     KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,  KC_RBRC,    KC_BSLS, KC_DEL,   KC_END,   KC_PGDN,
+     KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,  KC_VOLU,              KC_SNAP,  KC_SIRI,  RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,         KC_L,     KC_SCLN,      KC_QUOT,              KC_ENT,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,              KC_RSFT,           KC_UP,
-     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                    KC_RCMMD,     KC_ROPTN, MO(MAC_FN), KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,              KC_RSFT,            KC_UP,
+     KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                    KC_RCMMD,     KC_ROPTN, MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [MAC_FN] = LAYOUT_tkl_ansi(
-     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,   KC_F12,              KC_TRNS,  KC_TRNS,  RGB_TOG,
-     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_MAC,  RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,  RGB_M_G,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  DF(WIN_BASE), KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,   KC_F12,               KC_TRNS,  KC_TRNS,  RGB_TOG,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_MAC,  RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,  RGB_M_G,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  DF(WIN_BASE), KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,           RGB_TOG,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                   KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS, RGB_RMOD, RGB_TOG,  RGB_MOD),
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,            RGB_TOG,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                   KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD),
 
 [WIN_BASE] = LAYOUT_tkl_ansi(
-     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,   KC_F12,              KC_PSCR,  KC_SCRL,  KC_PAUS,
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,  KC_EQL,     KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,
-     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,  KC_RBRC,    KC_BSLS, KC_DEL,   KC_END,   KC_PGDN,
+     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,   KC_F12,               KC_PSCR,  KC_SCRL,  KC_PAUS,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,  KC_RBRC,    KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,    KC_K,         KC_L,     KC_SCLN,      KC_QUOT,              KC_ENT,
-     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,              KC_RSFT,           KC_UP,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                    KC_RALT,      KC_APP,   MO(WIN_FN), KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,    KC_M,         KC_COMM,  KC_DOT,       KC_SLSH,              KC_RSFT,            KC_UP,
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                    KC_RALT,      KC_APP,   MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_tkl_ansi(
-     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,  KC_VOLU,             KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_PC,   RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,  RGB_M_G,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
-     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_NO,        KC_TRNS,  KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,      CKC_TERM,             KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, DF(MAC_BASE), KC_TRNS,  KC_MYCM,      KC_CALC,              KC_TRNS,           RGB_TOG,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                   KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS, RGB_RMOD, RGB_TOG,  RGB_MOD)
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,  KC_VOLU,             CKC_TERM,  KC_MYCM,  KC_CALC,
+     KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  CKC_PC,   RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,  RGB_M_G,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_NO,        KC_TRNS,  KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, DF(MAC_BASE), KC_TRNS,  KC_TRNS,      KC_TRNS,              KC_TRNS,            RGB_TOG,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                   KC_TRNS,      KC_TRNS,  KC_TRNS,    KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD)
 
 };
 
@@ -75,28 +71,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CKC_MAC:
             if (record->event.pressed) {
-                // when keycode CKC_MAC is pressed
                 SEND_STRING("Mac");
-            } else {
-                // when keycode CKC_MAC is released
             }
             break;
 
         case CKC_PC:
             if (record->event.pressed) {
-                // when keycode CKC_PC is pressed
                 SEND_STRING("PC");
-            } else {
-                // when keycode CKC_PC is released
             }
             break;
 
         case CKC_TERM:
             if (record->event.pressed) {
-                // when keycode CKC_TERM is pressed
                 SEND_STRING(SS_LWIN("r") SS_DELAY(500) "WT" SS_TAP(X_ENT));
-            } else {
-                // when keycode CKC_TERM is released
             }
             break;
     }
@@ -105,14 +92,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 enum combo_events {
-    EM_WORK_EMAIL,
     EM_PERSONAL_EMAIL,
+    EM_WORK_EMAIL,
     COMBO_LENGTH
 };
 const uint16_t COMBO_LEN = COMBO_LENGTH;
 
-const uint16_t PROGMEM work_email_combo[] = {KC_E, KC_H, COMBO_END};
-const uint16_t PROGMEM personal_email_combo[] = {KC_E, KC_O, COMBO_END};
+const uint16_t PROGMEM personal_email_combo[] = {KC_E, KC_1, COMBO_END};
+const uint16_t PROGMEM work_email_combo[] = {KC_E, KC_2, COMBO_END};
 
 combo_t key_combos[] = {
     [EM_WORK_EMAIL] = COMBO_ACTION(work_email_combo),
@@ -122,16 +109,20 @@ combo_t key_combos[] = {
 void process_combo_event(uint16_t combo_index, bool pressed) {
 
     switch(combo_index) {
-        case EM_WORK_EMAIL:
-            if (pressed) {
-                SEND_STRING("me@work.com");
-            }
-            break;
-
         case EM_PERSONAL_EMAIL:
             if (pressed) {
                 SEND_STRING("me@home.com");
             }
             break;
+
+        case EM_WORK_EMAIL:
+            if (pressed) {
+                SEND_STRING("me@work.com");
+            }
+            break;
     }
+}
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
 }
