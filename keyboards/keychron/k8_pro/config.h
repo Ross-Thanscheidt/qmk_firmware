67b73e7284ad13b469574c686a259df7f6580b6b
Ross W. Thanscheidt <Ross_Thanscheidt@outlook.com>
2025-02-21 00:00:38 -0600

    Add Navigation Layer

--- keyboards/keychron/k8_pro/config.h
+++ keyboards/keychron/k8_pro/config.h
@@ -16,12 +16,6 @@
 
 #pragma once
 
-/* Activate Caps Word by double tapping Left Shift */
-#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
-
-/* Turn on NKRO by default */
-#define FORCE_NKRO
-
 /* Turn off effects when suspended */
 #define RGB_DISABLE_WHEN_USB_SUSPENDED
 #define LED_DISABLE_WHEN_USB_SUSPENDED
