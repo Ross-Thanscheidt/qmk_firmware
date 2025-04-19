28e0db4bd4b8b3b1af381a496a67dfca988ddc96
Ross W. Thanscheidt <Ross-Thanscheidt@outlook.com>
2025-04-18 19:46:13 -0500

    Fix Windows Terminal Profile Launching

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/Ross-Thanscheidt/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/Ross-Thanscheidt/keymap.c
@@ -54,12 +54,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,         KC_LOPTN, KC_LCMMD,                               KC_SPC,                                     KC_RCMMD,     KC_ROPTN,   MO(MAC_FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [MAC_FN] = LAYOUT_tkl_ansi(
-     KC_TRNS,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,     KC_F10,       KC_F11,     KC_F12,                 KC_TRNS,  KC_TRNS,  RGB_TOG,
-     KC_TRNS,         BT_HST1,  BT_HST2,  BT_HST3,  CKC_MAC,  RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN,  RGB_M_K,      RGB_M_X,    RGB_M_G,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     RGB_TOG,         RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   DF(WIN_BASE), KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,
-     KC_TRNS,                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, KC_NO,        KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,            RGB_TOG,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD),
+     _______,         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,     KC_F10,       KC_F11,     KC_F12,                 _______,  _______,  RGB_TOG,
+     _______,         BT_HST1,  BT_HST2,  BT_HST3,  CKC_MAC,  RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN,  RGB_M_K,      RGB_M_X,    RGB_M_G,      _______,  _______,  _______,  _______,
+     RGB_TOG,         RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______, _______,      _______,   DF(WIN_BASE), _______,    _______,      _______,  _______,  _______,  _______,
+     _______,         RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______, _______,      _______,   _______,      _______,                  _______,
+     _______,                   _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG, KC_NO,        _______,   _______,      _______,                  _______,            RGB_TOG,
+     _______,         _______,  _______,                                _______,                                    _______,      _______,    _______,      _______,  RGB_RMOD, RGB_TOG,  RGB_MOD),
 
 [WIN_BASE] = LAYOUT_tkl_ansi(
      KC_ESC,          KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,        KC_F9,     KC_F10,       KC_F11,     KC_F12,                 KC_PSCR,  KC_SCRL,  KC_PAUS,
@@ -70,28 +70,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL,         KC_LGUI,  KC_LALT,                                KC_SPC,                                     KC_RALT,      KC_APP,     MO(WIN_FN),   KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_tkl_ansi(
-     KC_TRNS,         KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,   KC_MUTE,      KC_VOLD,    KC_VOLU,                CKC_TERM, KC_MYCM,  KC_CALC,
-     KC_TRNS,         BT_HST1,  BT_HST2,  BT_HST3,  CKC_PC,   RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN,  RGB_M_K,      RGB_M_X,    RGB_M_G,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     RGB_TOG,         RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_NO,        KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,
-     KC_TRNS,                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG, DF(MAC_BASE), KC_TRNS,   KC_TRNS,      DF(KEYPAD),               KC_TRNS,            RGB_TOG,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  RGB_RMOD, RGB_TOG,  RGB_MOD),
+     _______,         KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,      KC_MNXT,   KC_MUTE,      KC_VOLD,    KC_VOLU,                CKC_TERM, KC_MYCM,  KC_CALC,
+     _______,         BT_HST1,  BT_HST2,  BT_HST3,  CKC_PC,   RGB_M_P,  RGB_M_B,  RGB_M_R, RGB_M_SW,     RGB_M_SN,  RGB_M_K,      RGB_M_X,    RGB_M_G,      _______,  _______,  _______,  _______,
+     RGB_TOG,         RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______, _______,      _______,   KC_NO,        _______,    _______,      _______,  _______,  _______,  _______,
+     _______,         RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______, _______,      _______,   _______,      _______,                  _______,
+     _______,                   _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG, DF(MAC_BASE), _______,   _______,      DF(KEYPAD),               _______,            RGB_TOG,
+     _______,         _______,  _______,                                _______,                                    _______,      _______,    _______,      _______,  RGB_RMOD, RGB_TOG,  RGB_MOD),
 
 [KEYPAD] = LAYOUT_tkl_ansi(
-     KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_NUM,   KC_PSLS,  KC_PAST, KC_PMNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,                KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  CKC_KP,   KC_TRNS,  KC_TRNS,  KC_KP_7, KC_KP_8,      KC_KP_9,   KC_PPLS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_4, KC_KP_5,      KC_KP_6,   KC_PPLS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_KP_1, KC_KP_2,      KC_KP_3,   KC_PENT,      KC_TRNS,                  KC_TRNS,
-     KC_TRNS,                   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_KP_0,   KC_PDOT,      KC_PENT,                  KC_TRNS,            KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    DF(WIN_BASE), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+     _______,         _______,  _______,  _______,  _______,  KC_NUM,   KC_PSLS,  KC_PAST, KC_PMNS,      _______,   _______,      _______,    _______,                _______,  _______,  _______,
+     _______,         _______,  _______,  _______,  CKC_KP,   _______,  _______,  KC_KP_7, KC_KP_8,      KC_KP_9,   KC_PPLS,      _______,    _______,      _______,  _______,  _______,  _______,
+     _______,         _______,  _______,  _______,  _______,  _______,  _______,  KC_KP_4, KC_KP_5,      KC_KP_6,   KC_PPLS,      _______,    _______,      _______,  _______,  _______,  _______,
+     _______,         _______,  _______,  _______,  _______,  _______,  _______,  KC_KP_1, KC_KP_2,      KC_KP_3,   KC_PENT,      _______,                  _______,
+     _______,                   _______,  _______,  _______,  _______,  _______,  _______, _______,      KC_KP_0,   KC_PDOT,      KC_PENT,                  _______,            _______,
+     _______,         _______,  _______,                                _______,                                    _______,      _______,    DF(WIN_BASE), _______,  _______,  _______,  _______),
 
 [NAV] = LAYOUT_tkl_ansi(
-     KC_TRNS,         CKC_WT1,  CKC_WT2,  CKC_WT3,  CKC_WT4,  CKC_WT5,  CKC_WT6,  CKC_WT7, CKC_WT8,      CKC_WT9,   CKC_WT0,      KC_TRNS,    KC_TRNS,                KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS, KC_INS,       KC_TRNS,   KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,         KC_HOME,  KC_NO,    KC_DEL,   KC_PGDN,  KC_TRNS,  KC_LEFT,  KC_DOWN, KC_UP,        KC_RGHT,   KC_TRNS,      KC_TRNS,                  KC_TRNS,
-     KC_TRNS,                   KC_END,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PGUP,  KC_TRNS, KC_TRNS,      KC_TRNS,   KC_TRNS,      KC_TRNS,                  KC_TRNS,            KC_TRNS,
-     KC_TRNS,         KC_TRNS,  KC_TRNS,                                KC_TRNS,                                    KC_TRNS,      KC_TRNS,    KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
+     _______,         _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,      _______,   _______,      _______,    _______,                _______,  _______,  _______,
+     _______,         CKC_WT1,  CKC_WT2,  CKC_WT3,  CKC_WT4,  CKC_WT5,  CKC_WT6,  CKC_WT7, CKC_WT8,      CKC_WT9,   CKC_WT0,      _______,    _______,      _______,  _______,  _______,  _______,
+     _______,         _______,  _______,  _______,  _______,  _______,  _______,  _______, KC_INS,       _______,   _______,      _______,    _______,      _______,  _______,  _______,  _______,
+     _______,         KC_HOME,  KC_NO,    KC_DEL,   KC_PGDN,  _______,  KC_LEFT,  KC_DOWN, KC_UP,        KC_RGHT,   _______,      _______,                  _______,
+     _______,                   KC_END,   _______,  _______,  _______,  KC_PGUP,  _______, _______,      _______,   _______,      _______,                  _______,            _______,
+     _______,         _______,  _______,                                _______,                                    _______,      _______,    _______,      _______,  _______,  _______,  _______)
 
 };
 
@@ -143,61 +143,61 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case CKC_WT1:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_1%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_1\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT2:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_2%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_2\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT3:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_3%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_3\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT4:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_4%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_4\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT5:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_5%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_5\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT6:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_6%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_6\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT7:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_7%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_7\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT8:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_8%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_8\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT9:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_9%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_9\%\"" SS_TAP(X_ENT));
             }
             break;
 
         case CKC_WT0:
             if (record->event.pressed) {
-                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "%WT% \"WT_0%\"" SS_TAP(X_ENT));
+                SEND_STRING(SS_LWIN("r") SS_DELAY(500) "\%WT\% \"\%WT_0\%\"" SS_TAP(X_ENT));
             }
             break;
     }
