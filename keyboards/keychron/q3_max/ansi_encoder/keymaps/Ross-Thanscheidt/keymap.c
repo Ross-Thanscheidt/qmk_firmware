766eb3b559378c839570ce734c84f09ddde8adcc
Ross W. Thanscheidt <Ross-Thanscheidt@outlook.com>
2025-04-30 23:12:58 -0500

    Add NAV+Q|W|E|S for Left|Up|Right|Down and Move Mac Function Keys to Layer 0

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/Ross-Thanscheidt/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/Ross-Thanscheidt/keymap.c
@@ -47,7 +47,7 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
-        KC_ESC,          KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,    KC_VOLU,      KC_MUTE,  KC_SNAP,  KC_SIRI, RGB_MOD,
+        KC_ESC,          KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,     KC_F12,       KC_MUTE,  KC_SNAP,  KC_SIRI, RGB_MOD,
         KC_GRV,          KC_1,     KC_2,    KC_3,     KC_4,     KC_5,    KC_6,    KC_7,    KC_8,         KC_9,     KC_0,         KC_MINS,    KC_EQL,       KC_BSPC,  KC_INS,   KC_HOME, KC_PGUP,
         KC_TAB,          KC_Q,     KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,         KC_O,     KC_P,         KC_LBRC,    KC_RBRC,      KC_BSLS,  KC_DEL,   KC_END,  KC_PGDN,
         LT(NAV,KC_CAPS), KC_A,     KC_S,    KC_D,     KC_F,     KC_G,    KC_H,    KC_J,    KC_K,         KC_L,     KC_SCLN,      KC_QUOT,                  KC_ENT,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,         KC_LOPTN, KC_LCMMD,                             KC_SPC,                                   KC_RCMMD,     KC_ROPTN,   MO(MAC_FN),   KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RGHT),
 
     [MAC_FN] = LAYOUT_tkl_ansi(
-        _______,         KC_F1,    KC_F2,   KC_F3,    KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,        KC_F9,    KC_F10,       KC_F11,     KC_F12,       RGB_TOG,  _______,  _______, RGB_TOG,
+        _______,         KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY,      KC_MNXT,  KC_MUTE,      KC_VOLD,    KC_VOLU,      RGB_TOG,  _______,  _______, RGB_TOG,
         _______,         BT_HST1,  BT_HST2, BT_HST3,  P2P4G,    RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW,     RGB_M_SN, RGB_M_K,      RGB_M_X,    RGB_M_G,      CKC_MAC,  _______,  _______, _______,
         RGB_TOG,         RGB_MOD,  RGB_VAI, RGB_HUI,  RGB_SAI,  RGB_SPI, _______, _______, _______,      _______,  DF(WIN_BASE), _______,    _______,      _______,  _______,  _______, _______,
         _______,         RGB_RMOD, RGB_VAD, RGB_HUD,  RGB_SAD,  RGB_SPD, _______, _______, _______,      _______,  _______,      _______,                  _______,
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NAV] = LAYOUT_tkl_ansi(
         _______,         _______,  _______, _______,  _______,  _______, _______, _______, _______,      _______,  _______,      _______,    _______,      KC_MUTE,  _______,  _______, _______,
         _______,         CKC_WT1,  CKC_WT2, CKC_WT3,  CKC_WT4,  CKC_WT5, CKC_WT6, CKC_WT7, CKC_WT8,      CKC_WT9,  CKC_WT0,      _______,    _______,      _______,  _______,  _______, _______,
-        _______,         _______,  _______, _______,  _______,  _______, _______, _______, KC_INS,       _______,  _______,      _______,    _______,      _______,  _______,  _______, _______,
-        _______,         KC_HOME,  KC_NO,   KC_DEL,   KC_PGDN,  _______, KC_LEFT, KC_DOWN, KC_UP,        KC_RIGHT, _______,      _______,                  _______,
+        _______,         KC_LEFT,  KC_UP,   KC_RIGHT, _______,  _______, _______, _______, KC_INS,       _______,  _______,      _______,    _______,      _______,  _______,  _______, _______,
+        _______,         KC_HOME,  KC_DOWN, KC_DEL,   KC_PGDN,  _______, KC_LEFT, KC_DOWN, KC_UP,        KC_RIGHT, _______,      _______,                  _______,
         _______,                   KC_END,  _______,  _______,  _______, KC_PGUP, _______, _______,      _______,  _______,      _______,                  _______,            _______,
         _______,         _______,  _______,                              _______,                                  _______,      _______,    _______,      _______,  _______,  _______, _______),
 
